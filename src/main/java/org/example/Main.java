174d13230c23650058c9de93a5575e65593b8ad8
Themis Darelis <thedarelis@gmail.com>
2026-03-05 13:47:37 +0200

    added student class

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setId(1);
+        s1.setFullName("Foufs");
+        s1.setAge(32);
+
+        System.out.println(s1.toString());
+
+
+    }
+}
